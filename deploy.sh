07576e16433594f69f11d0fdd8116a581185be8e
Aman King <amanking@gmail.com>
2013-03-26 23:08:53 +0530

    Fixed error message of deploy script to be more precise

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 status=`git status | head -2 | tail -1`
 if [ "nothing to commit (working directory clean)" != "$status" -a "-f" != "$1" ]; then
-  echo "ERROR: Please deploy only on a clean working directory"
+  echo "ERROR: Please deploy only on a clean working directory that has been pushed"
   exit 1
 fi
 git_sha=`git log -1 | head -1 | cut -f2 -d" "`
