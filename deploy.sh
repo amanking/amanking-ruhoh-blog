ecb452acb6b6be7afa8aa5d0f60e0fa725c37ae0
Aman King <amanking@gmail.com>
2013-03-26 23:06:51 +0530

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+status=`git status | head -2 | tail -1`
+if [ "nothing to commit (working directory clean)" != "$status" -a "-f" != "$1" ]; then
+  echo "ERROR: Please deploy only on a clean working directory"
+  exit 1
+fi
+git_sha=`git log -1 | head -1 | cut -f2 -d" "`
+bundle exec ruhoh compile && cd ../amanking.github.com/ && rm -Rf * && cp -r ../amanking-ruhoh-blog/compiled/* ./ && git add -A && git commit -m "Deploying $git_sha"
