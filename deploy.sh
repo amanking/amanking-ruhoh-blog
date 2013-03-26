1b9ce6e07461d266d7851999018ab5e22bcb3180
Aman King <amanking@gmail.com>
2013-03-26 23:12:06 +0530

    Deploy script will also push

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ if [ "nothing to commit (working directory clean)" != "$status" -a "-f" != "$1" 
   exit 1
 fi
 git_sha=`git log -1 | head -1 | cut -f2 -d" "`
-bundle exec ruhoh compile && cd ../amanking.github.com/ && rm -Rf * && cp -r ../amanking-ruhoh-blog/compiled/* ./ && git add -A && git commit -m "Deploying $git_sha"
+bundle exec ruhoh compile && cd ../amanking.github.com/ && rm -Rf * && cp -r ../amanking-ruhoh-blog/compiled/* ./ && git add -A && git commit -m "Deploying $git_sha" && git push && cd -
